871bc1ed63520589277cb38760b679447850cd83
Tirumarai Selvan <tiru@hasura.io>
2018-03-11 16:40:34 +0530

    update license

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2017 The Kubernetes Authors.
+# Copyright 2017 The Gitkube Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,22 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o errexit
-set -o nounset
-set -o pipefail
-
+#Run from project root
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/hasura/gitkube/pkg/client github.com/hasura/gitkube/pkg/apis \
-  gitkube.com:v1alpha1 \
-  --output-base "$(dirname ${BASH_SOURCE})/../../.." \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
-
-# To use your own boilerplate text use:
-#   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
+$SCRIPT_ROOT/vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
+github.com/hasura/gitkube/pkg/client github.com/hasura/gitkube/pkg/apis \
+gitkube.sh:v1alpha1 \
+--go-header-file $SCRIPT_ROOT/hack/boilerplate.go.txt
